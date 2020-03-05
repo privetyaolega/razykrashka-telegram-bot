f0375ba7249328757098ea6716adff02ef7d7cae
brokorenko <brokorenko@gmail.com>
2020-03-05 12:11:55 +0300

    My meeting section added. Liquibase script that uploads 3 users added

---
+++ src/main/java/com/razykrashka/bot/stage/meeting/view/utils/SendMessageUtils.java
@@ -0,0 +1,64 @@
+package com.razykrashka.bot.stage.meeting.view.utils;
+
+
+import com.razykrashka.bot.service.RazykrashkaBot;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+//TODO: Use StringBuilder where string concatenation happens
+
+@Component
+public class SendMessageUtils {
+/*    public SendMessage createSendMessageWithMeetings(Stage stage, List<Meeting> userMeetings, RazykrashkaBot razykrashkaBot) {
+        String meetingsText = createMeetingsText(userMeetings);
+        SendMessage sendMessage = createSendMessage(razykrashkaBot, meetingsText);
+        if (userMeetings.size() > 5) {
+            // PAGINATION INLINE KEYBOARD
+            sendMessage.setReplyMarkup(stage.getKeyboard(null));
+        }
+        return sendMessage;
+    }
+
+    private String createMeetingsText(List<Meeting> userMeetings) {
+        return userMeetings.stream().skip(0).limit(5)
+                .map(this::createSingleMeetingMainInformationText)
+                .collect(Collectors.joining("\n\n", "\uD83D\uDCAB Найдено " + userMeetings.size() + " встреч(и)\n\n", ""));
+    }
+
+    public SendMessage createSendMessageForSingleMeeting(Stage stage, Meeting userMeeting, RazykrashkaBot razykrashkaBot) {
+        String meetingText = createSingleMeetingText(userMeeting);
+        SendMessage sendMessage = createSendMessage(razykrashkaBot, meetingText);
+
+        sendMessage.setReplyMarkup(stage.getKeyboard(userMeeting));
+        return sendMessage;
+    }
+
+    private String createSingleMeetingText(Meeting meeting) {
+        return "<code>MEETING # " + meeting.getId() + "</code>\n" +
+                createSingleMeetingMainInformationText(meeting)
+                + meeting.getMeetingInfo().getQuestions() + "\n";
+    }
+
+    private SendMessage createSendMessage(RazykrashkaBot razykrashkaBot, String messageText) {
+        return new SendMessage()
+                .setChatId(razykrashkaBot.getUpdate().getMessage().getChat().getId())
+                .setParseMode("html")
+                .setText(messageText);
+    }
+
+    private String createSingleMeetingMainInformationText(Meeting meeting) {
+        return meeting.getMeetingDateTime().format(DateTimeFormatter.ofPattern("dd MMMM (EEEE) HH:mm",
+                Locale.ENGLISH)) + "\n"
+                + "\uD83D\uDCCD" + meeting.getLocation().getLocationLink().toString() + "\n"
+                + meeting.getMeetingInfo().getSpeakingLevel().toString() + "\n"
+                + meeting.getMeetingInfo().getTopic() + "\n"
+                + "INFORMATION: /meeting" + meeting.getId();
+    }*/
+
+}
