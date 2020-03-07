bd0d4f454d86753980cda8d831b8bd01a9f324ed
Aleh Rutski <privetyaolega@gmail.com>
2020-03-07 17:34:59 +0300

    SBS Level stage refactoring

--- src/main/java/com/razykrashka/bot/stage/MainStage.java
+++ src/main/java/com/razykrashka/bot/stage/MainStage.java
@@ -29,125 +29,129 @@ import java.util.Map;
 @AllArgsConstructor
 public abstract class MainStage implements Stage {
 
-	private static List<Map<String, Object>> data;
+    private static List<Map<String, Object>> data;
 
-	@Autowired
-	protected MeetingRepository meetingRepository;
-	@Autowired
-	protected TelegramUserRepository telegramUserRepository;
-	@Autowired
-	protected MeetingInfoRepository meetingInfoRepository;
-	@Autowired
-	protected LocationRepository locationRepository;
-	@Autowired
-	protected TelegramMessageRepository telegramMessageRepository;
+    @Autowired
+    protected MeetingRepository meetingRepository;
+    @Autowired
+    protected TelegramUserRepository telegramUserRepository;
+    @Autowired
+    protected MeetingInfoRepository meetingInfoRepository;
+    @Autowired
+    protected LocationRepository locationRepository;
+    @Autowired
+    protected TelegramMessageRepository telegramMessageRepository;
 
-	@Autowired
-	protected RazykrashkaBot razykrashkaBot;
+    @Autowired
+    protected RazykrashkaBot razykrashkaBot;
 
-	@Autowired
-	protected MessageManager messageManager;
-	@Autowired
-	protected KeyboardBuilder keyboardBuilder;
-	@Autowired
-	protected UpdateHelper updateHelper;
+    @Autowired
+    protected MessageManager messageManager;
+    @Autowired
+    protected KeyboardBuilder keyboardBuilder;
+    @Autowired
+    protected UpdateHelper updateHelper;
 
-	protected boolean stageActivity;
-	protected StageInfo stageInfo;
-	protected Update update;
+    protected boolean stageActivity;
+    protected StageInfo stageInfo;
+    protected Update update;
 
-	static {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
-		try {
-			data = mapper.readValue(new File("src/main/resources/stage/stageStringStorage.json"),
-					new TypeReference<List<Map<String, Map<String, String>>>>() {
-					});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    static {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        try {
+            data = mapper.readValue(new File("src/main/resources/stage/stageStringStorage.json"),
+                    new TypeReference<List<Map<String, Map<String, String>>>>() {
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	{
-		stageInfo = StageInfo.DEFAULT;
-	}
+    {
+        stageInfo = StageInfo.DEFAULT;
+    }
 
-	@Override
-	public void handleRequest() {
-		messageManager.sendSimpleTextMessage(stageInfo.getWelcomeMessageEn(), getKeyboard());
-	}
+    @Override
+    public void handleRequest() {
+        messageManager.sendSimpleTextMessage(stageInfo.getWelcomeMessageEn(), getKeyboard());
+    }
 
-	@Override
-	public StageInfo getStageInfo() {
-		return stageInfo;
-	}
+    @Override
+    public StageInfo getStageInfo() {
+        return stageInfo;
+    }
 
-	@Override
-	public boolean isStageActive() {
-		return updateHelper.isMessageTextEquals(this.getStageInfo().getKeyword());
-	}
+    @Override
+    public boolean isStageActive() {
+        return updateHelper.isMessageTextEquals(this.getStageInfo().getKeyword());
+    }
 
-	public InlineKeyboardMarkup getInlineRuEnKeyboard(String callBackData, String textButton) {
-		InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-		List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList();
-		keyboardButtonsRow1.add(new InlineKeyboardButton().setText(textButton)
-				.setCallbackData(stageInfo.getStageName() + callBackData));
+    public InlineKeyboardMarkup getInlineRuEnKeyboard(String callBackData, String textButton) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList();
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText(textButton)
+                .setCallbackData(stageInfo.getStageName() + callBackData));
 
-		inlineKeyboardMarkup.setKeyboard(Arrays.asList(keyboardButtonsRow1));
-		return inlineKeyboardMarkup;
-	}
+        inlineKeyboardMarkup.setKeyboard(Arrays.asList(keyboardButtonsRow1));
+        return inlineKeyboardMarkup;
+    }
 
-	@Override
-	public ReplyKeyboard getKeyboard() {
-		throw new RuntimeException("IMPLEMENT METHOD IN SPECIFIC CLASS.");
-	}
+    @Override
+    public ReplyKeyboard getKeyboard() {
+        throw new RuntimeException("IMPLEMENT METHOD IN SPECIFIC CLASS.");
+    }
 
-	@Override
-	public boolean processCallBackQuery() {
-		String callBackData = razykrashkaBot.getCallbackQuery().getData();
-		if (callBackData.equals(stageInfo.getStageName() + "en_ru")) {
-			messageManager.updateMessage(stageInfo.getWelcomeMessageRu(),
-					getInlineRuEnKeyboard("ru_en", "EN \uD83C\uDDFA\uD83C\uDDF8"));
-		}
-		if (callBackData.equals(stageInfo.getStageName() + "ru_en")) {
-			messageManager.updateMessage(stageInfo.getWelcomeMessageEn(),
-					getInlineRuEnKeyboard("en_ru", "RU \uD83C\uDDF7\uD83C\uDDFA"));
-		}
-		return true;
-	}
+    @Override
+    public boolean processCallBackQuery() {
+        String callBackData = razykrashkaBot.getCallbackQuery().getData();
+        if (callBackData.equals(stageInfo.getStageName() + "en_ru")) {
+            messageManager.updateMessage(stageInfo.getWelcomeMessageRu(),
+                    getInlineRuEnKeyboard("ru_en", "EN \uD83C\uDDFA\uD83C\uDDF8"));
+        }
+        if (callBackData.equals(stageInfo.getStageName() + "ru_en")) {
+            messageManager.updateMessage(stageInfo.getWelcomeMessageEn(),
+                    getInlineRuEnKeyboard("en_ru", "RU \uD83C\uDDF7\uD83C\uDDFA"));
+        }
+        return true;
+    }
 
-	protected String getCallBackString(String callBackData) {
-		return this.getClass().getSimpleName() + callBackData;
-	}
+    protected String getCallBackString(String callBackData) {
+        return this.getClass().getSimpleName() + callBackData;
+    }
 
-	@Override
-	public void setActive(boolean isActive) {
-		this.stageActivity = isActive;
-	}
+    @Override
+    public void setActive(boolean isActive) {
+        this.stageActivity = isActive;
+    }
 
-	protected Map<String, String> getStringMap() {
-		String className = this.getClass().getSimpleName();
-		return (Map<String, String>) data.stream()
-				.filter(x -> x.containsKey(className))
-				.findFirst().get().get(className);
-	}
+    protected Map<String, String> getStringMap() {
+        String className = this.getClass().getSimpleName();
+        return (Map<String, String>) data.stream()
+                .filter(x -> x.containsKey(className))
+                .findFirst().get().get(className);
+    }
 
-	public boolean getStageActivity() {
-		return stageActivity;
-	}
+    protected String getString(String key) {
+        return getStringMap().get(key);
+    }
 
-	protected void setActiveNextStage(Class clazz) {
-		razykrashkaBot.getStages().forEach(stage -> stage.setActive(false));
-		Stage stage = ((Stage) razykrashkaBot.getContext().getBean(clazz));
-		stage.setActive(true);
-	}
+    public boolean getStageActivity() {
+        return stageActivity;
+    }
 
-	protected String getPureCallBackData() {
-		if (razykrashkaBot.getRealUpdate().hasCallbackQuery()) {
-			return razykrashkaBot.getRealUpdate()
-					.getCallbackQuery().getData()
-					.replace(this.getClass().getSimpleName(), "");
-		}
-		throw new RuntimeException("UPDATE EXCEPTION: Update doesn't have Call Back Query!");
-	}
+    protected void setActiveNextStage(Class clazz) {
+        razykrashkaBot.getStages().forEach(stage -> stage.setActive(false));
+        Stage stage = ((Stage) razykrashkaBot.getContext().getBean(clazz));
+        stage.setActive(true);
+    }
+
+    protected String getPureCallBackData() {
+        if (razykrashkaBot.getRealUpdate().hasCallbackQuery()) {
+            return razykrashkaBot.getRealUpdate()
+                    .getCallbackQuery().getData()
+                    .replace(this.getClass().getSimpleName(), "");
+        }
+        throw new RuntimeException("UPDATE EXCEPTION: Update doesn't have Call Back Query!");
+    }
 }
