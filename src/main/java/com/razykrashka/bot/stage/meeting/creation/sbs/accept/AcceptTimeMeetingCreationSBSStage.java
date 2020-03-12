facfe6ade8973621c3f83f27f69711e3b309cd76
Aleh Rutski <privetyaolega@gmail.com>
2020-03-12 22:09:22 +0300

    General refactoring

--- src/main/java/com/razykrashka/bot/stage/meeting/creation/sbs/accept/AcceptTimeMeetingCreationSBSStage.java
+++ src/main/java/com/razykrashka/bot/stage/meeting/creation/sbs/accept/AcceptTimeMeetingCreationSBSStage.java
@@ -10,37 +10,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class AcceptTimeMeetingCreationSBSStage extends BaseMeetingCreationSBSStage {
 
-	private final static String TIME_REGEX = "^([0-1][0-9]|[2][0-3])[:-]([0-5][0-9])$";
-	private String timeMessage;
+    private final static String TIME_REGEX = "^([0-1][0-9]|[2][0-3])[:-]([0-5][0-9])$";
+    private String timeMessage;
 
-	@Override
-	public void handleRequest() {
-		timeMessage = updateHelper.getMessageText();
-		inputDataValidation();
+    @Override
+    public void handleRequest() {
+        timeMessage = updateHelper.getMessageText();
+        inputDataValidation();
 
-		messageManager.deleteLastMessage();
-		Meeting meeting = super.getMeetingInCreation();
-		meeting.setMeetingDateTime(meeting.getMeetingDateTime()
-				.withHour(Integer.parseInt(timeMessage.substring(0, 2)))
-				.withMinute(Integer.parseInt(timeMessage.substring(3))));
-		meetingRepository.save(meeting);
+        Meeting meeting = super.getMeetingInCreation();
+        meeting.setMeetingDateTime(meeting.getMeetingDateTime()
+                .withHour(Integer.parseInt(timeMessage.substring(0, 2)))
+                .withMinute(Integer.parseInt(timeMessage.substring(3))));
+        meetingRepository.save(meeting);
 
-		messageManager.deleteLastBotMessage();
-		razykrashkaBot.getContext().getBean(LocationMeetingCreationSBSStage.class).handleRequest();
-	}
+        messageManager.deleteLastMessage()
+                .deleteLastBotMessage();
+        razykrashkaBot.getContext().getBean(LocationMeetingCreationSBSStage.class).handleRequest();
+    }
 
-	private void inputDataValidation() {
-		if (!timeMessage.matches(TIME_REGEX)) {
-			String message = String.format(super.getStringMap().get("incorrectTimeFormat"), timeMessage);
-			messageManager.disableKeyboardLastBotMessage()
-					.replyLastMessage(message);
-			razykrashkaBot.getContext().getBean(TimeMeetingCreationSBSStage.class).handleRequest();
-			throw new IncorrectInputDataFormatException(timeMessage + ": incorrect time format!");
-		}
-	}
+    private void inputDataValidation() {
+        if (!timeMessage.matches(TIME_REGEX)) {
+            String message = String.format(super.getStringMap().get("incorrectTimeFormat"), timeMessage);
+            messageManager.disableKeyboardLastBotMessage()
+                    .replyLastMessage(message);
+            razykrashkaBot.getContext().getBean(TimeMeetingCreationSBSStage.class).handleRequest();
+            throw new IncorrectInputDataFormatException(timeMessage + ": incorrect time format!");
+        }
+    }
 
-	@Override
-	public boolean isStageActive() {
+    @Override
+    public boolean isStageActive() {
         return !updateHelper.hasCallBackQuery() && super.getStageActivity();
-	}
+    }
 }
