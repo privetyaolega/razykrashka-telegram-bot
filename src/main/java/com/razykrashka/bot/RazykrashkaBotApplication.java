51521633bc928463b9737cd569c3a3687d1d4222
privetyaolega <privetyaolega@gmail.com>
2020-03-23 00:55:42 +0300

    Tomcat building issue update

--- src/main/java/com/razykrashka/bot/RazykrashkaBotApplication.java
+++ src/main/java/com/razykrashka/bot/RazykrashkaBotApplication.java
@@ -2,14 +2,25 @@ package com.razykrashka.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class RazykrashkaBotApplication {
-    public static void main(String[] args) {
+public class RazykrashkaBotApplication extends SpringBootServletInitializer {
+
+    {
         ApiContextInitializer.init();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RazykrashkaBotApplication.class);
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(RazykrashkaBotApplication.class, args);
     }
 }
