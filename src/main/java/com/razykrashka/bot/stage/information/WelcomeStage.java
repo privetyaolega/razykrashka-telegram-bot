11be0f972062eb469fd615da6dd60b02c6f28fba
privetyaolega <privetyaolega@gmail.com>
2020-04-15 20:40:06 +0300

    WelcomeStage update

--- src/main/java/com/razykrashka/bot/stage/information/WelcomeStage.java
+++ src/main/java/com/razykrashka/bot/stage/information/WelcomeStage.java
@@ -4,13 +4,6 @@ import com.razykrashka.bot.stage.MainStage;
 import com.razykrashka.bot.stage.StageInfo;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Log4j2
 @Component
@@ -21,27 +14,12 @@ public class WelcomeStage extends MainStage {
     }
 
     @Override
-    public ReplyKeyboard getKeyboard() {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-        List<KeyboardRow> keyboard = new ArrayList<>();
-
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-        keyboardFirstRow.add(new KeyboardButton(StageInfo.SELECT_WAY_MEETING_CREATION.getKeyword()));
-        keyboardFirstRow.add(new KeyboardButton(StageInfo.MY_MEETING_VIEW.getKeyword()));
-        keyboardFirstRow.add(new KeyboardButton(StageInfo.SELECT_MEETINGS_TYPE.getKeyword()));
-
-        KeyboardRow keyboardSecondRow = new KeyboardRow();
-        keyboardSecondRow.add(new KeyboardButton(StageInfo.INFORMATION.getKeyword()));
-
-        keyboard.add(keyboardFirstRow);
-        keyboard.add(keyboardSecondRow);
-        replyKeyboardMarkup.setKeyboard(keyboard);
-
-        return replyKeyboardMarkup;
+    public void handleRequest() {
+        messageManager.sendSimpleTextMessage(getString("intro"), getMainKeyboard());
     }
 
+    @Override
+    public boolean isStageActive() {
+        return updateHelper.isMessageContains("/start");
+    }
 }
