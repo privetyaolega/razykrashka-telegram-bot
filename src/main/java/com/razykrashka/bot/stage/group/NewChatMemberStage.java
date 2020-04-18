3bb7573fafbf05bb47517e3c16edfdb13fb915de
privetyaolega <privetyaolega@gmail.com>
2020-04-18 12:21:34 +0300

    NewChatMemberStage notification for each new user

--- src/main/java/com/razykrashka/bot/stage/group/NewChatMemberStage.java
+++ src/main/java/com/razykrashka/bot/stage/group/NewChatMemberStage.java
@@ -1,25 +1,40 @@
 package com.razykrashka.bot.stage.group;
 
 
-import com.razykrashka.bot.db.entity.razykrashka.TelegramUser;
 import com.razykrashka.bot.stage.MainStage;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Component
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class NewChatMemberStage extends MainStage {
 
     @Override
     public void handleRequest() {
-        TelegramUser user = updateHelper.getUser();
-        messageManager.sendMessage(new SendMessage()
-                .setChatId(String.valueOf(user.getId()))
-                .setText(super.getString("welcome")));
+        List<User> newChatMembers = updateHelper.getUpdate()
+                .getMessage()
+                .getNewChatMembers();
+        log.info("New chat members: {}", getNewMembersString(newChatMembers));
+        newChatMembers.forEach(m -> messageManager.sendMessage(new SendMessage()
+                .setChatId(String.valueOf(m.getId()))
+                .setText(getString("welcome"))));
+    }
+
+    private String getNewMembersString(List<User> newChatMembers) {
+        return newChatMembers.stream()
+                .map(this::getUserString)
+                .collect(Collectors.joining(",", "[", "]"));
+    }
+
+    private String getUserString(User u) {
+        String userName = Optional.ofNullable(u.getUserName()).isPresent() ? u.getUserName() : "";
+        return u.getId() + " " + userName;
     }
 
     @Override
